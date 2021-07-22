c66cec80a0e9046324801c0ef06800e560a18b51
tutuh <63507813+tutuh@users.noreply.github.com>
2021-07-22 13:36:23 +0800

    Update caiyun.js

--- JS/caiyun.js
+++ JS/caiyun.js
@@ -4,7 +4,7 @@
  * Author Github: https://github.com/blackmatrix7
  * [Script]
  * 彩云天气_SVIP = type=http-response,requires-body=1,max-size=0,pattern=https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user),script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/caiyun/caiyun_svip.js
- * 
+ * QX :  https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user) url script-response-body https://raw.githubusercontent.com/tutuh/script/master/JS/caiyun.js
  */
 
 const SCRIPT_NAME = '彩云天气';
