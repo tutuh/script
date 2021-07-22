f4eff5b233b32630f428809fbc9073151be267f8
tutuh <63507813+tutuh@users.noreply.github.com>
2021-07-22 14:17:31 +0800

    Update VUE.js

--- JS/VUE.js
+++ JS/VUE.js
@@ -1,12 +1,21 @@
 /*
 ---BY NobyDa---
-VUE Vlog app unlocks pro
-Surge4.0:
-http-response https:\/\/api\.vuevideo\.net\/api\/v1\/(users\/.+\/profile|subtitle\/prepare) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
-QX 1.0.0:
-^https:\/\/api\.vuevideo\.net\/api\/v1\/(users\/.+\/profile|subtitle\/prepare) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
-Surge & QX Mitm = api.vuevideo.net
-*/
 
-body = $response.body.replace(/\"isPremium\":false/, "\"isPremium\":true").replace(/\"valid\":false/, "\"valid\":true")
-$done({body});
+VUE Vlog 解锁高级功能 (需登录)
+***************************
+QuantumultX:
+[rewrite_local]
+^https:\/\/api\.vuevideo\.net\/api\/v1\/(users\/.+\/profile|subtitle\/prepare) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
+[mitm]
+hostname = api.vuevideo.net
+***************************
+Surge4 or Loon:
+[Script]
+http-response https:\/\/api\.vuevideo\.net\/api\/v1\/(users\/.+\/profile|subtitle\/prepare) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
+[MITM]
+hostname = api.vuevideo.net
+**************************/
+var body = $response.body
+    .replace(/\"isPremium\":false/, "\"isPremium\":true")
+    .replace(/\"valid\":false/, "\"valid\":true");
+$done({ body });
