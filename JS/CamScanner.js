e9297f67b817fe92fe41c4f3e997943f1b4e0a3e
tutuh <63507813+tutuh@users.noreply.github.com>
2021-07-22 14:14:23 +0800

    Update CamScanner.js

--- JS/CamScanner.js
+++ JS/CamScanner.js
@@ -1,14 +1,25 @@
 /*
 ---BY NobyDa---
-CamScanner unlocks pro, Cloud scanning is not available.
-QuanX 1.0.0:  [rewrite_local]
+CamScanner 解锁部分高级特权
+***************************
+Quantumult X:
+[rewrite_local]
 ^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
-Quan:  [REWRITE]
-https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
-Surge4.0: [Script]
+[mitm]
+hostname = ap*.intsig.net
+***************************
+Surge4 or Loon:
+[Script]
 http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
-QX & Q & Surge Mitm = ap*.intsig.net,
-*/
+[MITM]
+hostname = ap*.intsig.net
+***************************
+Quantumult:  
+[REWRITE]
+https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
+[MITM]
+hostname = ap*.intsig.net
+**************************/
 
 let obj = JSON.parse($response.body);
 obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
