cc4af1235ff33bcd2f405a2ded28bf5ba61e48b8
tutuh <63507813+tutuh@users.noreply.github.com>
2021-07-23 12:27:18 +0800

    Update PdfExpert.js

--- JS/PdfExpert.js
+++ JS/PdfExpert.js
@@ -1,3 +1,10 @@
+/*
+BY langkhach270389
+# PdfExpert vip  license.pdfexpert.com
+QX    ^https:\/\/license\.pdfexpert\.com\/api\/.+\/subscription\/(refresh$|check$) url script-response-body https://raw.githubusercontent.com/langkhach270389/Surge-LK/main/scripts/langkhach/pdfexpert.js
+Surge  Documents-PdfExpert = type=http-response,pattern=^https:\/\/license\.pdfexpert\.com\/api\/.+\/subscription\/(refresh$|check$),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/langkhach270389/Surge-LK/main/scripts/langkhach/pdfexpert.js,script-update-interval=-1
+*/
+
 re('\\w{1,2}\/\\w{1,2}\/\\w{4}@"subscriptionState":"\\w+"@"is(.+)User":false','25/11/2099@"subscriptionState":"active"@"is$1User":true');
 
 function re() {
