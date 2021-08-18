ae992ed202380827dc95b2487ce1e0af8437d504
tutuh <63507813+tutuh@users.noreply.github.com>
2021-08-18 16:09:30 +0800

    Create photoshop.js

---
+++ JS/photoshop.js
@@ -0,0 +1,7 @@
+//BY langkhach270389
+let obj = JSON.parse($response.body)
+let pro= obj["mobileProfile"];
+pro["profileStatus"] = "PROFILE_AVAILABLE";
+pro["legacyProfile"] = "{}";
+pro["relationshipProfile"] = "[]";
+$done({body: JSON.stringify(obj)})
