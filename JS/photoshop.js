2f81f8456b7aa4a915db9e2529966d0a95f9a888
tutuh <63507813+tutuh@users.noreply.github.com>
2021-08-20 12:48:39 +0800

    Delete JS directory

--- JS/photoshop.js
+++
@@ -1,7 +0,0 @@
-//BY langkhach270389
-let obj = JSON.parse($response.body)
-let pro= obj["mobileProfile"];
-pro["profileStatus"] = "PROFILE_AVAILABLE";
-pro["legacyProfile"] = "{}";
-pro["relationshipProfile"] = "[]";
-$done({body: JSON.stringify(obj)})
