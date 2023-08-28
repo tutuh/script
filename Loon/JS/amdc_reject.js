cd3a37c2457063b52592a6719dadfe962d221737
tutuh <63507813+tutuh@users.noreply.github.com>
2023-08-28 11:27:31 +0800

    Update amdc_reject.js

--- Loon/JS/amdc_reject.js
+++ Loon/JS/amdc_reject.js
@@ -1,7 +1,7 @@
 /*
-引用地址https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amdc.js
+引用地址https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/header.js
 */
-// 2023-08-22 15:45
+// 2023-08-24 12:45
 
 const url = $request.url;
 const header = $request.headers;
@@ -11,6 +11,7 @@ let ua = header["User-Agent"] || header["user-agent"];
 if (url.includes("/amdc/mobileDispatch")) {
   if (
     ua.includes("AMapiPhone") || // 高德地图
+    ua.includes("Alibaba") || // 阿里巴巴
     ua.includes("Cainiao4iPhone") || // 菜鸟
     ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C") // 飞猪旅行
   ) {
