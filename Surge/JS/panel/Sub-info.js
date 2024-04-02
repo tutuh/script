c1ff78a5006fd648e5c9b796d9817160a1f514e6
tutuh <63507813+tutuh@users.noreply.github.com>
2024-04-02 16:13:13 +0800

    Update Sub-info.js

--- Surge/JS/panel/Sub-info.js
+++ Surge/JS/panel/Sub-info.js
@@ -35,7 +35,13 @@
       content.push(`到期：${formatTime(args.expire || info.expire)}`);
     }
   }
-
+  
+  let now = new Date();
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  hour = hour > 9 ? hour : "0" + hour;
+  minutes = minutes > 9 ? minutes : "0" + minutes;
+  
   $done({
     title: `${args.title} | ${hour}:${minutes}`,
     content: content.join("\n"),
