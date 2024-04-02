90c739aead14289445b5a6e604956482a6399782
tutuh <63507813+tutuh@users.noreply.github.com>
2024-04-02 16:24:07 +0800

    Update Sub-info.js

--- Surge/JS/panel/Sub-info.js
+++ Surge/JS/panel/Sub-info.js
@@ -36,11 +36,17 @@
     }
   }
 
+  let now = new Date();
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  hour = hour > 9 ? hour : "0" + hour;
+  minutes = minutes > 9 ? minutes : "0" + minutes;
+
   $done({
-    title: `${args.title}`,
+    title: `${args.title} | ${hour}:${minutes}`,
     content: content.join("\n"),
-    icon: args.icon || "tornado",
-    "icon-color": args.color || "#DF4688",
+    icon: args.icon || "airplane.circle",
+    "icon-color": args.color || "#007aff",
   });
 })();
 
