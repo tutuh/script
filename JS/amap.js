d992077733f62a41c445fa2c724608a3dbb894f7
tutuh <63507813+tutuh@users.noreply.github.com>
2024-09-05 12:47:34 +0800

    Update amap.js

--- JS/amap.js
+++ JS/amap.js
@@ -1,5 +1,5 @@
 // By RuCu6
-// 2024-08-31 17:15
+// 2024-09-03 21:40
 
 const url = $request.url;
 if (!$response.body) $done({});
@@ -181,6 +181,10 @@ if (url.includes("/aos/perception/publicTravel/beforeNavi")) {
   // 成就勋章 lv1见习达人
   if (obj?.data?.memberInfo) {
     delete obj.data.memberInfo;
+  }
+  if (obj?.data?.topMixedCard) {
+    // 顶部足迹、贡献卡片
+    delete obj.data.topMixedCard;
   }
 } else if (url.includes("/shield/frogserver/aocs/updatable/")) {
   // 整体图层
