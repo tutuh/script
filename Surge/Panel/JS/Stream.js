9b8230c45fd3d7161f9d3dd11687cc9be94ee709
tutuh <63507813+tutuh@users.noreply.github.com>
2026-05-21 10:19:18 +0800

    更新 Stream.js

--- Surge/Panel/JS/Stream.js
+++ Surge/Panel/JS/Stream.js
@@ -74,7 +74,7 @@ function getCurrentTime() {
   const timeStr = getCurrentTime();
   
   const panel = {
-    title: `${baseTitle}    <${timeStr}>`, // 在这里拼接时间显示
+    title: `${baseTitle}　　<${timeStr}>`, // 这里用了两个全角空格
     content: '',
     icon: args.icon || 'play.tv.fill',
     'icon-color': args.color || '#D22F20'
